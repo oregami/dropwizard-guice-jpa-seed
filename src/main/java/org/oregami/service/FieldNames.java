f81d5b7732cb16ce7afeac5ccfcb1a1f2edcbdda
sebeichholz <gene@oregami.org>
2014-09-22 17:46:41 +0200

    subtask validation

--- src/main/java/org/oregami/service/FieldNames.java
+++ src/main/java/org/oregami/service/FieldNames.java
@@ -6,6 +6,8 @@ public interface FieldNames {
 	public static final String TASK_DESCRIPTION = "task.description";
 	public static final String TASK_FINISHED = "task.finished";
 
+    public static final String SUBTASK_DESCRIPTION = "subtask.description";
+
     public static final String LANGUAGE_NAME = "language.name";
     public static final String LANGUAGE_DESCRIPTION = "language.description";
 	
