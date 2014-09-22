b18e9829aaf357f2b366281999537048726f8be4
sebeichholz <gene@oregami.org>
2014-09-22 17:45:07 +0200

    subtask validation

---
+++ src/main/java/org/oregami/validation/SubTaskValidator.java
@@ -0,0 +1,60 @@
+package org.oregami.validation;
+
+import org.apache.commons.lang.StringUtils;
+import org.oregami.entities.SubTask;
+import org.oregami.entities.Task;
+import org.oregami.entities.TaskDao;
+import org.oregami.service.FieldNames;
+import org.oregami.service.ServiceError;
+import org.oregami.service.ServiceErrorContext;
+import org.oregami.service.ServiceErrorMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubTaskValidator {
+
+    private final SubTask subTask;
+
+    private final int descriptionMinLenght = 3;
+
+    public SubTaskValidator(SubTask subTask) {
+        if (subTask == null) {
+            throw new RuntimeException("org.oregami.taskvalidator.NoSubTaskGiven");
+        }
+        this.subTask = subTask;
+    }
+
+    public List<ServiceError> validateForCreation() {
+        List<ServiceError> errors = new ArrayList<ServiceError>();
+
+        errors.addAll(validateRequiredFields());
+
+        return errors;
+
+    }
+
+    public List<ServiceError> validateRequiredFields() {
+        List<ServiceError> errorMessages = new ArrayList<ServiceError>();
+
+        if (StringUtils.isEmpty(subTask.getDescription())) {
+            errorMessages.add(
+                    new ServiceError(
+                            new ServiceErrorContext(FieldNames.SUBTASK_DESCRIPTION, subTask.getId()),
+                            ServiceErrorMessage.SUBTASK_DESCRIPTION_EMPTY
+                    )
+            );
+        }
+        else if (StringUtils.length(subTask.getDescription()) < descriptionMinLenght) {
+        	errorMessages.add(
+                    new ServiceError(
+                            new ServiceErrorContext(FieldNames.SUBTASK_DESCRIPTION, subTask.getId()),
+                            ServiceErrorMessage.SUBTASK_DESCRIPTION_TOO_SHORT
+                    )
+            );
+        }
+
+        return errorMessages;
+    }
+
+}
---
+++ src/test/java/org/oregami/test/TestSubTaskValidator.java
@@ -0,0 +1,81 @@
+package org.oregami.test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.persist.PersistService;
+import com.google.inject.persist.jpa.JpaPersistModule;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.oregami.dropwizard.ToDoApplication;
+import org.oregami.entities.SubTask;
+import org.oregami.entities.Task;
+import org.oregami.entities.TaskDao;
+import org.oregami.service.FieldNames;
+import org.oregami.service.ServiceError;
+import org.oregami.service.ServiceErrorContext;
+import org.oregami.service.ServiceErrorMessage;
+import org.oregami.validation.TaskValidator;
+
+import java.util.List;
+
+public class TestSubTaskValidator {
+
+	static Injector injector = null;
+	
+	ServiceError errorDescriptionEmpty = new ServiceError(new ServiceErrorContext(FieldNames.SUBTASK_DESCRIPTION), ServiceErrorMessage.SUBTASK_DESCRIPTION_EMPTY);
+    ServiceError errorDescriptionTooShort = new ServiceError(new ServiceErrorContext(FieldNames.SUBTASK_DESCRIPTION), ServiceErrorMessage.SUBTASK_DESCRIPTION_TOO_SHORT);
+
+    @BeforeClass
+	public static void init() {
+		JpaPersistModule jpaPersistModule = new JpaPersistModule(ToDoApplication.JPA_UNIT);
+		injector = Guice.createInjector(jpaPersistModule);
+		injector.getInstance(PersistenceTest.class);
+		PersistService persistService = injector.getInstance(PersistService.class);
+		persistService.start();
+	}
+
+    private Task createValidTask() {
+        Task t = new Task("valid task name");
+        t.setDescription("This is a description");
+        return t;
+    }
+
+
+    @Test
+    public void testNoErrors() {
+        Task t = createValidTask();
+        SubTask subTask = new SubTask();
+        subTask.setDescription("This is a valid subtask description");
+        t.addSubTask(subTask);
+
+        TaskValidator validator = new TaskValidator(injector.getInstance(TaskDao.class), t);
+
+        List<ServiceError> errors = validator.validateForCreation();
+        System.out.println(errors.toString());
+
+        Assert.assertTrue(errors.isEmpty());
+
+    }
+
+	@Test
+	public void testDescriptionTooShort() {
+		Task t = createValidTask();
+        SubTask subTask = new SubTask();
+        t.addSubTask(subTask);
+
+        TaskValidator validator = new TaskValidator(injector.getInstance(TaskDao.class), t);
+
+		List<ServiceError> errors = validator.validateForCreation();
+		System.out.println(errors.toString());
+
+		Assert.assertFalse(errors.isEmpty());
+        Assert.assertTrue(errors.size()==1);
+
+		Assert.assertTrue(errors.contains(errorDescriptionEmpty));
+		
+	}	
+	
+	
+	
+}
