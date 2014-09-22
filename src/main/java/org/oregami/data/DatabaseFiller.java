df09464629592fa7132c3e0d5b94014e4a3d911e
sebeichholz <gene@oregami.org>
2014-09-22 17:44:19 +0200

    more test data

--- src/main/java/org/oregami/data/DatabaseFiller.java
+++ src/main/java/org/oregami/data/DatabaseFiller.java
@@ -10,9 +10,9 @@ import com.google.inject.persist.Transactional;
 import com.google.inject.persist.jpa.JpaPersistModule;
 import org.oregami.entities.Language;
 import org.oregami.entities.LanguageDao;
+import org.oregami.entities.SubTask;
 import org.oregami.entities.Task;
 import org.oregami.service.LanguageService;
-import org.oregami.service.ServiceError;
 import org.oregami.service.ServiceResult;
 import org.oregami.service.TaskService;
 
@@ -60,12 +60,26 @@ public class DatabaseFiller {
     private void addTasks() {
         TaskService taskService = injector.getInstance(TaskService.class);
         LanguageDao languageDao = injector.getInstance(LanguageDao.class);
-        Task t1 = new Task("task1");
+        Task t1 = new Task("task 1");
         t1.setDescription("This is task 1");
         t1.setLanguage(languageDao.findByExactName("english"));
+
+        SubTask sub1 = new SubTask();
+        sub1.setDescription("this a subtask 1");
+        t1.addSubTask(sub1);
+
         ServiceResult<Task> taskServiceResult = taskService.createNewTask(t1);
         if (taskServiceResult.hasErrors()) {
             throw new RuntimeException(taskServiceResult.getErrors().toString());
         }
+
+        Task t2 = new Task("task 2");
+        t2.setDescription("This is task 2");
+        t2.setLanguage(languageDao.findByExactName("english"));
+        taskServiceResult = taskService.createNewTask(t2);
+        if (taskServiceResult.hasErrors()) {
+            throw new RuntimeException(taskServiceResult.getErrors().toString());
+        }
+
     }
 }
