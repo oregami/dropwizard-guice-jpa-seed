cf24a3f30d01e356ae3df714d0543c19523bcbd4
sebeichholz <gene@oregami.org>
2014-09-22 17:47:08 +0200

    add id to ServiceErrorContext

--- src/test/java/org/oregami/service/TestServiceErrorContext.java
+++ src/test/java/org/oregami/service/TestServiceErrorContext.java
@@ -12,8 +12,15 @@ public class TestServiceErrorContext {
 		ServiceErrorContext c2 = new ServiceErrorContext("test");
 		
 		Assert.assertTrue(c1.hashCode()==c2.hashCode());
-		
 	}
+
+    @Test
+    public void testHashCodeEqualId() {
+        ServiceErrorContext c1 = new ServiceErrorContext("test", "id1");
+        ServiceErrorContext c2 = new ServiceErrorContext("test", "id1");
+
+        Assert.assertTrue(c1.hashCode()==c2.hashCode());
+    }
 	
 	@Test
 	public void testHashCodeNotEqual() {
@@ -22,16 +29,41 @@ public class TestServiceErrorContext {
 		
 		Assert.assertTrue(c1.hashCode()!=c2.hashCode());
 	}
-	
-	
-	@Test
+
+    @Test
+    public void testHashCodeNotEqualId() {
+        ServiceErrorContext c1 = new ServiceErrorContext("test", "id1");
+        ServiceErrorContext c2 = new ServiceErrorContext("other", "id1");
+
+        Assert.assertTrue(c1.hashCode()!=c2.hashCode());
+    }
+
+    @Test
+    public void testHashCodeNotEqualId2() {
+        ServiceErrorContext c1 = new ServiceErrorContext("test", "id1");
+        ServiceErrorContext c2 = new ServiceErrorContext("test", "id2");
+
+        Assert.assertTrue(c1.hashCode()!=c2.hashCode());
+    }
+
+
+    @Test
 	public void testEquals() {
 		ServiceErrorContext c1 = new ServiceErrorContext("test");
 		ServiceErrorContext c2 = new ServiceErrorContext("test");
 		
 		Assert.assertTrue(c1.equals(c1));
 		Assert.assertTrue(c1.equals(c2));
-	}	
+	}
+
+    @Test
+    public void testEqualsId() {
+        ServiceErrorContext c1 = new ServiceErrorContext("test", "id1");
+        ServiceErrorContext c2 = new ServiceErrorContext("test", "id1");
+
+        Assert.assertTrue(c1.equals(c1));
+        Assert.assertTrue(c1.equals(c2));
+    }
 	
 	
 	@Test
@@ -40,8 +72,24 @@ public class TestServiceErrorContext {
 		ServiceErrorContext c2 = new ServiceErrorContext("other");
 		
 		Assert.assertFalse(c1.equals(c2));
-	}	
-	
+	}
 
-	
+    @Test
+    public void testNotEqualsId() {
+        ServiceErrorContext c1 = new ServiceErrorContext("test", "id1");
+        ServiceErrorContext c2 = new ServiceErrorContext("other", "id1");
+
+        Assert.assertFalse(c1.equals(c2));
+    }
+
+    @Test
+    public void testNotEqualsId2() {
+        ServiceErrorContext c1 = new ServiceErrorContext("test", "id1");
+        ServiceErrorContext c2 = new ServiceErrorContext("test", "id2");
+
+        Assert.assertFalse(c1.equals(c2));
+    }
+
+
+
 }
