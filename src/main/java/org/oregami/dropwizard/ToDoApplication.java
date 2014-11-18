d912bda6c1a2c2944af8df02b73e81859ca3f980
sebeichholz <gene@oregami.org>
2014-11-18 22:48:09 +0100

    Json Web Token authentication

--- src/main/java/org/oregami/dropwizard/ToDoApplication.java
+++ src/main/java/org/oregami/dropwizard/ToDoApplication.java
@@ -1,24 +1,32 @@
 package org.oregami.dropwizard;
 
-import io.dropwizard.Application;
-import io.dropwizard.auth.basic.BasicAuthProvider;
-import io.dropwizard.setup.Bootstrap;
-import io.dropwizard.setup.Environment;
-
-import java.util.EnumSet;
-
-import javax.servlet.DispatcherType;
-import javax.servlet.FilterRegistration.Dynamic;
-
-import org.eclipse.jetty.servlets.CrossOriginFilter;
-import org.oregami.data.DatabaseFiller;
-import org.oregami.resources.TaskResource;
-import org.oregami.user.User;
-
+import com.github.toastshaman.dropwizard.auth.jwt.JWTAuthProvider;
+import com.github.toastshaman.dropwizard.auth.jwt.JsonWebTokenParser;
+import com.github.toastshaman.dropwizard.auth.jwt.JsonWebTokenValidator;
+import com.github.toastshaman.dropwizard.auth.jwt.hmac.HmacSHA512Verifier;
+import com.github.toastshaman.dropwizard.auth.jwt.model.JsonWebToken;
+import com.github.toastshaman.dropwizard.auth.jwt.parser.DefaultJsonWebTokenParser;
+import com.github.toastshaman.dropwizard.auth.jwt.validator.ExpiryValidator;
+import com.google.common.base.Optional;
 import com.google.inject.persist.PersistFilter;
 import com.google.inject.persist.PersistService;
 import com.google.inject.persist.jpa.JpaPersistModule;
 import com.hubspot.dropwizard.guice.GuiceBundle;
+import io.dropwizard.Application;
+import io.dropwizard.auth.AuthenticationException;
+import io.dropwizard.auth.Authenticator;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
+import org.oregami.data.DatabaseFiller;
+import org.oregami.resources.SecuredResource;
+import org.oregami.resources.TaskResource;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration.Dynamic;
+import java.security.SecureRandom;
+import java.util.EnumSet;
+import java.util.Random;
 
 public class ToDoApplication extends Application<ToDoConfiguration> {
 
@@ -26,12 +34,14 @@ public class ToDoApplication extends Application<ToDoConfiguration> {
             "data";
 	        // "dataMysql";
 
+  public static byte[] authKey;
+
 	private static final JpaPersistModule jpaPersistModule = new JpaPersistModule(JPA_UNIT);
 
 	private GuiceBundle<ToDoConfiguration> guiceBundle;
-	
+
 	PersistService persistService = null;
-	
+
 	public static void main(String[] args) throws Exception {
 		new ToDoApplication().run(args);
 	}
@@ -53,10 +63,19 @@ public class ToDoApplication extends Application<ToDoConfiguration> {
 //		persistService = guiceBundle.getInjector().getInstance(PersistService.class);
 //      persistService.start();
 		DatabaseFiller.getInstance().initData();
-		
+
+    initAuthKey();
+
 	}
 
-	@Override
+  private void initAuthKey() {
+    Random random = new SecureRandom();
+    ToDoApplication.authKey = new byte[64];
+    random.nextBytes(ToDoApplication.authKey);
+    System.out.println("AuthKey initialized!");
+  }
+
+  @Override
 	public void run(ToDoConfiguration configuration, Environment environment) {
 
         environment.servlets().addFilter("persistFilter", guiceBundle.getInjector().getInstance(PersistFilter.class)).addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), true, "/*");
@@ -69,20 +88,55 @@ public class ToDoApplication extends Application<ToDoConfiguration> {
         filter.setInitParameter(CrossOriginFilter.EXPOSED_HEADERS_PARAM, "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,Location");
 	    filter.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,Location");
 	    filter.setInitParameter(CrossOriginFilter.ALLOW_CREDENTIALS_PARAM, "true");
-//	    
+//
 //	    filter.setInitParameter("allow", "GET,PUT,POST,DELETE,OPTIONS");
 //	    filter.setInitParameter("preflightMaxAge", "5184000"); // 2 months
-		
 
-        environment.jersey().register(guiceBundle.getInjector().getInstance(TaskResource.class));
-		
-		environment.jersey().register(new BasicAuthProvider<User>(new ToDoBasicAuthenticator(),
-                "only visible with valid user/password"));
-		
+      environment.jersey().register(guiceBundle.getInjector().getInstance(TaskResource.class));
+
+
+
+
+    final JsonWebTokenParser tokenParser = new DefaultJsonWebTokenParser();
+    final HmacSHA512Verifier tokenVerifier = new HmacSHA512Verifier(ToDoApplication.authKey);
+    final JsonWebTokenValidator expiryValidator = new ExpiryValidator();
+    environment.jersey().register(new JWTAuthProvider<User>(new Authenticator<JsonWebToken, User>() {
+      @Override
+      public Optional<User> authenticate(JsonWebToken token) throws AuthenticationException {
+        // Provide your own implementation to lookup users based on the principal attribute in the
+        // JWT Token. E.g.: lookup users from a database etc.
+        // This method will be called once the token's signature has been verified
+
+        // In case you want to verify different parts of the token you can do that here.
+        // E.g.: Verifying that the provided token has not expired.
+       /*
+        try {
+
+          expiryValidator.validate(token);
+        } catch (TokenExpiredException e) {
+          throw new AuthenticationException(e.getMessage(), e);
+        }
+        */
+
+        if ("good-guy".equals(token.claim().getParameter("principal"))) {
+          return Optional.of(new User("good-guy"));
+        }
+
+        return Optional.absent();
+      }
+    }, tokenParser, tokenVerifier, "realm"));
+
+    environment.jersey().register(new SecuredResource(ToDoApplication.authKey));
+
+
+    //environment.jersey().register(new SecureResource());
+
+
+
 	}
-	
+
 	public static JpaPersistModule createJpaModule() {
 		return jpaPersistModule;
-	}	
+	}
 
 }
